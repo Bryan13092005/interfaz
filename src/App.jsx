193721650fcc6031c678f8124824f0a8c504ec93
Mila1213 <tu_correo@ejemplo.com>
2026-01-24 21:36:07 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home fundaciones={fundaciones} />} />
-        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to="/login" />} />
+        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to="/interfaz" />} />
         <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
         <Route path="/registro" element={user ? <Navigate to="/dashboard" /> : <Register />} />
         <Route path="*" element={<Navigate to="/" />} />
