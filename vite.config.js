8db0bd9cb2bfdfb8ae847f0302348c8fb9757574
Mila1213 <tu_correo@ejemplo.com>
2026-02-01 14:25:19 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/interfaz/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'docs'
