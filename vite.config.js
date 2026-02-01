f7cb6610e5480f4eb327fe2b559e9e4d9356841f
Mila1213 <tu_correo@ejemplo.com>
2026-02-01 14:02:30 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // si tu repo se llama "interfaz", pon /interfaz/
-  // para publicar en https://<usuario>.github.io/interfaz/
-  base: '/interfaz/',
+
+  base: '/',
   plugins: [react()],
   build: {
-    // hace que el resultado vaya a la carpeta docs para GitHub Pages (opcional)
+ 
     outDir: 'docs'
   }
 })
