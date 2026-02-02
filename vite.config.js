17fac1c9cf89ae47372ef6315ca3ba8b6463088b
Bryan13092005 <bryan.salvador@epn.edu.ec>
2026-02-01 22:32:03 -0500

    Deploys

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/interfaz/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/interfaz/' : '/',
   plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: mode === 'github' ? 'docs' : 'dist'
   }
-})
+}))
