222914da1f52616e7ec8a414e1d6824108202ce1
Mila1213 <tu_correo@ejemplo.com>
2026-02-01 20:46:25 -0500

    arreglos

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -8,25 +8,20 @@ import Promesa from "../Promesa/Promesa";
 import Contact from "../Contact/Contact";
 import Footer from "../Footer/Footer";
 import "./Landing.css";
-// Y así con los demás... (ONG, Fundaciones, Contact, Footer)
+
 function Landing() {
   return (
     <div className="landing">
       <Header />
-      <Hero />
-      <ONG />
+      <Promesa/>
+      <Hero/>
+      <ONG/>
       <Fundaciones />
       <FundacionesNuevas />
-      <Promesa />
       <Contact />
       <Footer />
 
-      <Link to="/login">
-       <button className="btn-login">Ir al Login</button>
-       </Link>
-       <Link to="/registro">
-       <button className="btn-registro">Ir al Registro</button>
-       </Link>
+  
 
     </div>
   );
